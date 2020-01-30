2195cfd1c2c4a95c5102e2efef6c2823c1cb5ca8
Jamani <jamanifowler5@gmail.com>
2020-01-29 22:03:59 -0800

    FINAL

--- NBA Player Mash-Up/ViewController.swift
+++ NBA Player Mash-Up/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let answers = ["Stephen", "Dwayne", "Kobe", "Dwight", "Kevin", "John", "Paul", "Chris", "Anthony", "Kyrie", "James", "Demarcus", "Joel", "Charles", "Michael", "Shaquille", "Isaiah", "Russell"]
+    
+    let last = ["James", "Curry", "Bryant", "Howard", "Durant", "Wall", "George", "Paul", "Davis", "Irving", "Harden", "Cousins", "Embiid", "Barkley", "Jordan", "O' Neal", "Thomas", "Westbrick"]
+    
+    
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var FirstLabel: UILabel!
+    @IBOutlet weak var LastLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateFirst()
+        generateLast()
+        
+    }
+    func generateFirst() {
+        let randomIndex = Int.random(in: 0..<answers.count)
+        
+        FirstLabel.text = answers[randomIndex]
+        
+        }
+        func generateLast() {
+        let randomLastIndex = Int.random(in: 0..<last.count)
+        
+        LastLabel.text = last [randomLastIndex]
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+    guard motion == .motionShake else { return }
+
+        generateFirst()
+        generateLast()
+
 
 }
-
+}
